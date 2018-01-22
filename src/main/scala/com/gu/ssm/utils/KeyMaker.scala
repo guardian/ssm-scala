08e354ff7a6207314db2cfb4f5d09b5b844a5cbc
Justin Rowles <justin.rowles.casual@guardian.co.uk>
2018-01-22 14:23:23 +0000

    Addressing feedback

--- src/main/scala/com/gu/ssm/utils/KeyMaker.scala
+++ src/main/scala/com/gu/ssm/utils/KeyMaker.scala
@@ -5,14 +5,14 @@ import org.bouncycastle.util.io.pem.PemObject
 import org.bouncycastle.util.io.pem.PemWriter
 import java.security.Key
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.security.KeyPairGenerator
+import java.security.Security
+
 import org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey
 import org.apache.commons.codec.binary.Base64
 
 object KeyMaker {
-
-  import org.bouncycastle.jce.provider.BouncyCastleProvider
-  import java.security.KeyPairGenerator
-  import java.security.Security
 
   def makeKey (file: File, algorithm: String, provider: String): String = {
     Security.addProvider(new BouncyCastleProvider)
@@ -20,7 +20,7 @@ object KeyMaker {
     val priv = keyPair.getPrivate
     val pub = keyPair.getPublic
     writePemFile(priv, "RSA PRIVATE KEY", file)
-    new AuthorizedKey(pub, "security_ssm-scala").toString
+    toAuthorizedKey(pub, "security_ssm-scala")
   }
 
   private def generateKeyPair(algorithm: String, provider: String) = {
@@ -30,36 +30,30 @@ object KeyMaker {
     keyPair
   }
 
-  private def writePemFile(key: Key, description: String, file: File): Unit = {
-    new PemFile(key, description, file).write
+  private def toAuthorizedKey(key: Key, description: String) = {
+    val rsaPublicKey = key.asInstanceOf[BCRSAPublicKey]
+    val byteOs: ByteArrayOutputStream = new ByteArrayOutputStream
+    val dos = new DataOutputStream(byteOs)
+    dos.writeInt ("ssh-rsa".getBytes.length)
+    dos.write ("ssh-rsa".getBytes)
+    dos.writeInt (rsaPublicKey.getPublicExponent.toByteArray.length)
+    dos.write (rsaPublicKey.getPublicExponent.toByteArray)
+    dos.writeInt (rsaPublicKey.getModulus.toByteArray.length)
+    dos.write (rsaPublicKey.getModulus.toByteArray)
+    val publicKeyEncoded = new String (Base64.encodeBase64 (byteOs.toByteArray) )
+    "ssh-rsa " + publicKeyEncoded + " " + description
   }
-}
 
-class PemFile(val key: Key, val description: String, val file: File) {
-  private val pemObject = new PemObject(description, key.getEncoded)
-
-  def write(): Unit = {
+  private def writePemFile(key: Key, description: String, file: File) {
+    val pemObject = new PemObject(description, key.getEncoded)
     val pemWriter = new PemWriter(new OutputStreamWriter(new FileOutputStream(file)))
-    pemWriter.writeObject(this.pemObject)
+    pemWriter.writeObject(pemObject)
     pemWriter.close()
   }
+
 }
 
 
 
 
-class AuthorizedKey(val key: Key, val description: String) {
-  override def toString: String = {
-    val rsaPublicKey = key.asInstanceOf[BCRSAPublicKey]
-      val byteOs: ByteArrayOutputStream = new ByteArrayOutputStream
-      val dos = new DataOutputStream(byteOs)
-      dos.writeInt ("ssh-rsa".getBytes.length)
-      dos.write ("ssh-rsa".getBytes)
-      dos.writeInt (rsaPublicKey.getPublicExponent.toByteArray.length)
-      dos.write (rsaPublicKey.getPublicExponent.toByteArray)
-      dos.writeInt (rsaPublicKey.getModulus.toByteArray.length)
-      dos.write (rsaPublicKey.getModulus.toByteArray)
-      val publicKeyEncoded = new String (Base64.encodeBase64 (byteOs.toByteArray) )
-      "ssh-rsa " + publicKeyEncoded + " " + description
-  }
-}
+
