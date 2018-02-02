ecd438c465684e68f26aced29e2aff1beb619a52
Pascal Honoré <pascal@alseyn.net>
2018-02-02 15:50:18 +0000

    Better test names

--- src/test/scala/com/gu/ssm/MainTest.scala
+++ src/test/scala/com/gu/ssm/MainTest.scala
@@ -7,12 +7,12 @@ import com.gu.ssm.Main.computeIncorrectInstances
 class MainTest extends FreeSpec with Matchers with EitherValues {
 
   "computeIncorrectInstances" - {
-    "compute correct difference 1" in {
+    "should return empty list when matching list of Instance Ids" in {
       val executionTarget = ExecutionTarget(Some(List(InstanceId("i-096fdd62fd48b5b99"))))
       val results = List((InstanceId("i-096fdd62fd48b5b99"), Right(CommandResult("", "")) ))
       computeIncorrectInstances(executionTarget, results) shouldEqual Nil
     }
-    "compute correct difference 2" in {
+    "should return incorrectly submitted Instance Id" in {
       val executionTarget = ExecutionTarget(Some(List(InstanceId("i-096fdd62fd48b5b99"),InstanceId("i-12345"))))
       val results = List((InstanceId("i-096fdd62fd48b5b99"), Right(CommandResult("", "")) ))
       computeIncorrectInstances(executionTarget, results) shouldEqual List(InstanceId("i-12345"))
