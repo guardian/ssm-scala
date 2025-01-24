3deb566f57c9f7466aee6a2d4ca9de7279890c4a
adamnfish <adam.fisher@guardian.co.uk>
2025-01-24 16:46:56 +0000

    Match the Scala version in the generate-executable script

--- generate-executable.sh
+++ generate-executable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCALA_FOLDER="scala-3.6.2"
+SCALA_FOLDER="scala-3.6.3"
 cd $DIR
 sbt assembly
 cat "$DIR/generate-executable-prefix" "$DIR/target/$SCALA_FOLDER/ssm.jar" > "$DIR/target/$SCALA_FOLDER/ssm"
